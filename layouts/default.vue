a890df7d992b6e195f45cb0d942ef5b155942649
Tristan Timblin <tatimblin@gmail.com>
2019-03-18 19:53:40 -0400

    init

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+h1 {
+  color: $red;
+}
+</style>
---
+++ pages/styles.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="styles">
+        <div class="text container">
+            <h1>This is a heading one.</h1>
+            <h2>This is a heading two.</h2>
+            <h3>This is a heading three.</h3>
+            <h4>This is a heading four.</h4>
+            <h5>This is a heading five.</h5>
+            <h6>This is a heading six.</h6>
+            <p>As a man was passing the elephants, he suddenly stopped, confused by the fact that these huge creatures were being held by only a small rope tied to their front leg. No chains, no cages. It was obvious that the elephants could, at anytime, break away from their bonds but for some reason, they did not.</p>
+            <p>He saw a trainer nearby and asked why these animals just stood there and made no attempt to get away. “Well,” trainer said, “when they are very young and much smaller we use the same size rope to tie them and, at that age, it’s enough to hold them. As they grow up, they are conditioned to believe they cannot break away. They believe the rope can still hold them, so they never try to break free.”</p>
+        </div>
+        <div class="container">
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+            </ul>
+            <ol>
+                <li>One</li>
+                <li>Two</li>
+            </ol>
+        </div>
+    </section>
+</template>
